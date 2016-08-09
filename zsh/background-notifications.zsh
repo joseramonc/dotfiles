9e1c30fcd2deab5a7d55bbdfaa544457eac67326
Jose Ramon Camacho <joserracamacho@gmail.com>
2016-08-09 10:09:09 -0500

    :art:

---
+++ zsh/background-notifications.zsh
@@ -0,0 +1,8 @@
+function f_notifyme {
+  LAST_EXIT_CODE=$?
+  CMD=$(fc -ln -1)
+  # No point in waiting for the command to complete
+  notifyme "$CMD" "$LAST_EXIT_CODE" &
+}
+
+export PS1='$(f_notifyme)'$PS1
