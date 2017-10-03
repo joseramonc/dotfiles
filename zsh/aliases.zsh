4e9b5e000299aa28fe789a74ef1bde06671cd940
Jose Ramon Camacho <joserracamacho@gmail.com>
2017-10-03 11:35:23 -0500

    :art:

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,4 +1,4 @@
 alias reload!='. ~/.zshrc'
 alias mate='mate .'
 alias atom='atom . -n'
-alias ios="open /Applications/Xcode.app/Contents/Applications/iOS\ Simulator.app"
+alias ios="open /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app"
