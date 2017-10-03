4f0d28b7823538c150a517a3d7cc3c4fd7288bbe
Jose Ramon Camacho <joserracamacho@gmail.com>
2017-10-03 11:35:35 -0500

    fix high sierra

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -87,4 +87,4 @@ defaults write com.apple.messageshelper.MessageController SOInputLineSettings -d
 defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
 
 # Disable local Time Machine backups
-hash tmutil &> /dev/null && sudo tmutil disablelocal
+# hash tmutil &> /dev/null && sudo tmutil disablelocal
