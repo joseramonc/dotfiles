35deeb07803bfca23efa68404656224cbdb6911e
Jose Ramon Camacho <joserracamacho@gmail.com>
2020-10-01 18:04:17 -0500

    add duplicate lines

--- sublime.symlink/close_other_tabs_command.py
+++ sublime.symlink/close_other_tabs_command.py
@@ -1,12 +1,12 @@
 import sublime, sublime_plugin
 
 class CloseOtherTabsCommand(sublime_plugin.WindowCommand):
-  def run(self):
-    window = self.window
-    active_group = window.active_group()
-    curr_view_id = window.active_view_in_group(active_group).id()
+    def run(self):
+        window = self.window
+        active_group = window.active_group()
+        curr_view_id = window.active_view_in_group(active_group).id()
 
-    for v in window.views_in_group(active_group):
-      if v.id() == curr_view_id: continue
-      window.focus_view(v)
-      window.run_command("close")
+        for v in window.views_in_group(active_group):
+            if v.id() == curr_view_id: continue
+                window.focus_view(v)
+                window.run_command("close")
---
+++ sublime.symlink/duplicate_lines_command.py
@@ -0,0 +1,32 @@
+import sublime, sublime_plugin
+from sublime import Region
+
+class DuplicateLinesCommand(sublime_plugin.TextCommand):
+    def run(self, edit, up = False):
+        last_caret_region = [(selection.begin(), selection.end()) for selection in self.view.sel()]
+        for region in self.view.sel():
+            if not region.empty():
+                if "\n" in self.view.substr(region)[-1]:
+                    region = Region(region.begin(), region.end() - 1)
+
+            line = self.view.line(region)
+            line_contents = self.view.substr(line)
+
+            if up:
+              self.view.insert(edit, line.end(), "\n" + line_contents)
+            else:
+              self.view.insert(edit, line.begin(), line_contents + "\n")
+
+        if up:
+            self.view.sel().clear()
+            for selection in last_caret_region:
+                self.view.sel().add(sublime.Region(selection[0], selection[1]))
+
+
+
+# # Test
+# # A
+# # B
+# # C
+# # D
+# # E
